102b910a4e31f39c1ba95959486a8e4d0542c7a0
hiimani@28 <himani412.t@gmail.com>
2025-02-10 22:54:08 -0330

    board arcs

--- pacman.py
+++ pacman.py
@@ -1,11 +1,12 @@
 import pygame 
 from board import boards
-
+import math
 pygame.init()
 
 
 WIDTH = 800
 HEIGHT = 750
+PI = math.pi
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 fps = 60 # max speed at which game could be played. (no advantage even with high speed comp)
@@ -27,9 +28,30 @@ def draw_board():
                 pygame.draw.line(screen,color,(j*num2 + (0.5*num2), i*num1), # x coordinate for line
                                 (j*num2 + (0.5*num2), i*num1 + num1), 3  ) # y coordinate, from the top to the bottom of the square --> i*num1 + num1. 
                 
-            if level[i][j] == 4:
-                pygame.draw.line(screen,color,(j*num2, i*num1+(0.5*num1)), # vertical lines 
+
+            
+                
+            if level[i][j] == 4: # vertical lines 
+                pygame.draw.line(screen,color,(j*num2, i*num1+(0.5*num1)), 
                                 (j*num2 + num2, i*num1 + (0.5*num1)), 3  )
+                
+            if level[i][j] == 5: # left arc
+                pygame.draw.arc(screen, color, [(j*num2  - (num2*0.4)) - 2, (i*num1 + (0.5*num1)), num2, num1],0, PI/2, 3)
+                # A full circle is 0 to 2PI (radiance), but for arc its going to be 1/4th of a circle, which is basically pi/2
+            
+            if level[i][j] == 6: # right arc
+                pygame.draw.arc(screen, color, [(j*num2  + (num2*0.5)) , (i*num1 + (0.5*num1)), num2, num1],PI/2, PI, 3)
+            
+            if level[i][j] == 7: 
+                pygame.draw.arc(screen, color, [(j*num2  + (num2*0.5)), (i*num1 - (0.4*num1)), num2, num1],PI, 3*PI/2, 3)
+            
+            if level[i][j] == 8: 
+                pygame.draw.arc(screen, color, [(j*num2  + (num2*0.4)) -2 , (i*num1 - (0.4*num1)), num2, num1],3*PI/2, 2*PI, 3)
+            
+
+            if level[i][j] == 9: # white horizontal line
+                pygame.draw.line(screen,'white',(j*num2, i*num1+(0.5*num1)), # vertical lines 
+                    (j*num2 + num2, i*num1 + (0.5*num1)), 3  ) 
 
 
 run = True # game loop to execute the game
