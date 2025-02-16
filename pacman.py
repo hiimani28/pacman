8ac858fac8e414fca76a6d02cd2414fbea8ace6a
hiimani@28 <himani412.t@gmail.com>
2025-02-16 08:34:05 -0330

    turning directions

--- pacman.py
+++ pacman.py
@@ -21,6 +21,9 @@ player_y =  500
 direction = 0 
 counter = 0 
 flicker = False
+valid_turn = False
+# 0: Right, 1: left,2:Up, 3: Down
+turns_allowed = [False, False, False, False]
 def draw_board(): 
     num1 = ((HEIGHT - 50)//32) # how tall each piece of the board should be. 
     num2 = (WIDTH//30) # 30 horizontal blocks 
@@ -68,6 +71,59 @@ def draw_player():
     elif direction == 3: # up
         screen.blit(pygame.transform.rotate(player_images[counter//5], 270),(player_x,player_y))
 
+def check_positions(centerx, centery): # allowed diretions from a position 
+    turns = [False, False, False, False]
+    num1 = (HEIGHT-50)//32
+    num2 = (WIDTH//30) # need to check if the player center position is clear above and below on the sides 
+    num3 = 15   #fudge factor 
+    # 0,1,2 -> allowed but the remaining are not (blue lines/arcs)
+
+    """Understanding the calculations : 
+    HEIGHT = 640
+    num1 = (640 - 50) // 32 = 590 // 32 = 18 
+    num1 represents the height of each grid cell and num2 represents the width of each grid cell
+     :divided into 30 columns and 32 rows
+    If centery = 200 pixels:
+    200 // 18 = 11  (so the player is in row 11 of the grid)
+
+    (centerx - num3) // num2
+    centerx -> player's x poisition (pixels)
+     num2 -> width of each cell
+     centerx//num2 -> gives the column index in the level grid
+    WIDTH = 900
+    num2 = 900 // 30 = 30
+    450 // 30 = 15  (so the player is in column 15 of the grid)
+    applying a fudge factor makes is 450-15 // 30 , which is 14.    
+    """
+
+    """check collisions based on center x and center y of the player +/- fudge factor. Since every number depicts
+     a shape inside a square (for example 3),
+    player can go first half of the square. but when they touch the blue line, they're not able to do so."""
+
+    if centerx // 30 < 29: # Turn backwards where you came from
+        if direction == 0: # right
+            if level[centery//num1][(centerx - num3)//num2] <3: #space behind me is empty, small big dot
+                turns[1]= True
+
+
+            if direction == 1: #left
+                if level[centery//num1][(centerx + num3)//num2] <3: 
+                    turns[0]= True
+            
+            if direction == 2: #up
+                if level[centery+num3//num1][(centerx)//num2] <3: #checking underneath, (0,0) is top left.
+                    turns[3]= True
+            
+            if direction == 3: #down
+                if level[centery-num3][(centerx - num3)//num2] <3:
+                    turns[2]= True
+
+
+    else: 
+        turns[0] = True
+        turns[1] = True
+
+    return turns
 
 run = True # game loop to execute the game
 while run:
@@ -84,6 +140,11 @@ while run:
     screen.fill('black') # solid bg color
     draw_board()
     draw_player()
+    center_x = player_x + 22
+    center_y = player_y + 16
+    # pygame.draw.circle(screen,'white', (center_x, center_y),2)
+     
+    valid_turn = check_positions(center_x, center_y)
 
 #get out of the game
     for event in pygame.event.get(): #event game handler  
